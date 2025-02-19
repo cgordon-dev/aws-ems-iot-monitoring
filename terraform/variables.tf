eef83e1443dc18b2a83b51a247a37ddf9387b888
cgordon-dev <cgordon.dev@gmail.com>
2025-02-18 23:33:47 -0500

    Initial commit: Upload complete AWS EMS IoT Monitoring project

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -4,4 +4,20 @@ variable "region" {
   default     = "us-east-1"
 }
 
-# You can add additional variables for resource names if needed.
+variable "iot_thing_name" {
+  description = "Name of the AWS IoT Thing"
+  type        = string
+  default     = "ems-monitoring-device"
+}
+
+variable "iot_policy_name" {
+  description = "Name of the AWS IoT Policy"
+  type        = string
+  default     = "ems_iot_policy"
+}
+
+variable "dynamodb_table_name" {
+  description = "Name of the DynamoDB table to store sensor data"
+  type        = string
+  default     = "SensorData"
+}
